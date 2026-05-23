adb8be481cbc2c5a3d70b638d22caea8acd5650a
Ahmed Omara <ahmedhfmo@gmail.com>
2026-05-23 04:18:11 +0300

    add clients logic

---
+++ lib/feature/my_products/presentation/widgets/empty_products_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+import '../../../../generated/assets.dart';
+
+class EmptyProductsWidget extends StatelessWidget {
+  const EmptyProductsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Lottie.asset(
+        Assets.json.empty,
+        height: MediaQuery.sizeOf(context).height * 0.30,
+        fit: BoxFit.fill
+    );
+  }
+}
