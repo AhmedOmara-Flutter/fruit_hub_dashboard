46d04a67dce298c71d7186e0b2f24f622049993b
Ahmed Omara <ahmedhfmo@gmail.com>
2026-06-17 09:39:10 +0300

    handle main

---
+++ lib/core/src/src.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:fruit_hub_dashboard/core/repos/orders_repo/orders_repo.dart';
+import 'package:fruit_hub_dashboard/core/repos/reviews_repo/review_repo_impl.dart';
+import 'package:fruit_hub_dashboard/core/services/database_services.dart';
+import 'package:fruit_hub_dashboard/core/utils/theme_manager.dart';
+import 'package:fruit_hub_dashboard/feature/clients/data/repos/clients_repo_impl.dart';
+import 'package:fruit_hub_dashboard/feature/clients/presentation/view_model/clients_cubit.dart';
+import 'package:fruit_hub_dashboard/feature/reviews/presentation/view_model/get_products_with_review/get_product_with_reviews_cubit.dart';
+import 'package:fruit_hub_dashboard/feature/reviews/presentation/view_model/get_reviews/get_reviews_cubit.dart';
+import '../../feature/main/presentation/view_model/main_cubit.dart';
+import '../../feature/offers/data/repos/offer_repo_impl.dart';
+import '../../generated/l10n.dart';
+import '../cubit/offers_cubit/offers_cubit.dart';
+import '../cubit/orders_cubit/orders_cubit.dart';
+import '../cubit/products_cubit/products_cubit.dart';
+import '../repos/product_repo/product_repo_impl.dart';
+import '../utils/route_manager.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => MainCubit()),
+        BlocProvider(create: (context) =>
+            ClientsCubit(ClientsRepoImpl(FirestoreDatabase()),
+              OrdersRepoImpl(FirestoreDatabase()),
+            )),
+        BlocProvider(create: (context) =>
+            GetProductWithReviewsCubit(ReviewRepoImpl(FirestoreDatabase()))),
+        BlocProvider(create: (context) =>
+            GetReviewsCubit(ReviewRepoImpl(FirestoreDatabase()))),
+        BlocProvider(create: (context) =>
+            ProductsCubit(ProductRepoImpl(
+              FirestoreDatabase(),
+            ))),
+        BlocProvider(create: (context) =>
+            OffersCubit(OfferRepoImpl(FirestoreDatabase()),
+                ProductRepoImpl(FirestoreDatabase()))),
+        BlocProvider(create: (context) =>
+            OrdersCubit(OrdersRepoImpl(FirestoreDatabase()))),
+      ],
+      child: MaterialApp(
+        localizationsDelegates: [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: S.delegate.supportedLocales,
+        locale: Locale('ar'),
+        theme: ThemeManager.lightTheme,
+        debugShowCheckedModeBanner: false,
+        onGenerateRoute: GenerateRoute.generateRoute,
+        initialRoute: RouteManager.splash,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:fruit_hub_dashboard/core/services/database_services.dart';
-import 'package:fruit_hub_dashboard/core/utils/theme_manager.dart';
-import 'package:fruit_hub_dashboard/feature/reviews/presentation/view_model/get_reviews/get_reviews_cubit.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'core/cubit/offers_cubit/offers_cubit.dart';
-import 'core/cubit/orders_cubit/orders_cubit.dart';
-import 'core/cubit/products_cubit/products_cubit.dart';
-import 'core/repos/orders_repo/orders_repo.dart';
-import 'core/repos/product_repo/product_repo_impl.dart';
-import 'core/utils/route_manager.dart';
-import 'feature/clients/data/repos/clients_repo_impl.dart';
-import 'feature/clients/presentation/view_model/clients_cubit.dart';
-import 'feature/main/presentation/view_model/main_cubit.dart';
-import 'feature/offers/data/repos/offer_repo_impl.dart';
-import 'core/repos/reviews_repo/review_repo_impl.dart';
-import 'feature/reviews/presentation/view_model/get_products_with_review/get_product_with_reviews_cubit.dart';
+
+import 'core/src/src.dart';
 import 'firebase_options.dart';
-import 'generated/l10n.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -33,46 +17,3 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => MainCubit()),
-        BlocProvider(create: (context) =>
-            ClientsCubit(ClientsRepoImpl(FirestoreDatabase()),
-                OrdersRepoImpl(FirestoreDatabase()),
-                )),
-        BlocProvider(create: (context) =>
-            GetProductWithReviewsCubit(ReviewRepoImpl(FirestoreDatabase()))),
-        BlocProvider(create: (context) =>
-            GetReviewsCubit(ReviewRepoImpl(FirestoreDatabase()))),
-        BlocProvider(create: (context) =>
-            ProductsCubit(ProductRepoImpl(
-              FirestoreDatabase(),
-            ))),
-        BlocProvider(create: (context) =>
-            OffersCubit(OfferRepoImpl(FirestoreDatabase()),
-                ProductRepoImpl(FirestoreDatabase()))),
-        BlocProvider(create: (context) =>
-            OrdersCubit(OrdersRepoImpl(FirestoreDatabase()))),
-      ],
-      child: MaterialApp(
-        localizationsDelegates: [
-          S.delegate,
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-        ],
-        supportedLocales: S.delegate.supportedLocales,
-        locale: Locale('ar'),
-        theme: ThemeManager.lightTheme,
-        debugShowCheckedModeBanner: false,
-        onGenerateRoute: GenerateRoute.generateRoute,
-        initialRoute: RouteManager.splash,
-      ),
-    );
-  }
-}
