40ecab8f3f5e4edf675f0a9112d854b5e5b62343
Ahmed Omara <ahmedhfmo@gmail.com>
2026-07-30 19:02:05 +0300

    finish add printer to project

--- lib/core/services/print_service.dart
+++ lib/core/services/print_service.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'dart:io';
-
 import '../entities/order_entity.dart';
 
 class PrintService {
@@ -8,107 +7,44 @@ class PrintService {
     final subTotal = order.cartEntity.getTotalPrice();
     final shipping = order.selectedLocationEntity?.cost ?? 0.0;
     final total = subTotal + shipping;
-
-
-    // عدد المنتجات المختلفة
     final itemCount = order.cartEntity.cartItems.length;
-
-
-    // إجمالي الكميات
     final totalQuantity = order.cartEntity.cartItems.fold(
       0,
-          (sum, item) => sum + item.quantity,
+      (sum, item) => sum + item.quantity,
     );
-
-
     final json = {
       "OrderNumber": order.id ?? "",
-
-      "CustomerName":
-      order.userEntity?.userName ?? "",
-
-      "Phone":
-      order.userEntity?.phone ?? "",
-
-
-      "Address":
-      order.addressEntity?.address ?? "",
-
-      "DeliveryArea":
-      order.selectedLocationEntity?.title ?? "",
-
-
+      "CustomerName": order.userEntity?.userName ?? "",
+      "Phone": order.userEntity?.phone ?? "",
+      "Address": order.addressEntity?.address ?? "",
+      "DeliveryArea": order.selectedLocationEntity?.title ?? "",
       "SubTotal": subTotal,
-
       "Shipping": shipping,
-
       "Total": total,
-
-
-      "OrderNote":
-      order.orderNote ?? "",
-
-
-      "PaymentMethod":
-      (order.isCashOnDelivery ?? true)
-          ? "Cash"
-          : "Online",
-
-
-      // الجديد
-      "OrderDate":
-      DateTime.now().toIso8601String(),
-
-
-      "ItemCount":
-      itemCount,
-
-
-      "TotalQuantity":
-      totalQuantity,
-
-
-      "Items":
-      order.cartEntity.cartItems
-          .map((e) => {
-        "Name": e.product.name,
-        "Quantity": e.quantity,
-        "Price": e.unitPrice,
-      })
+      "OrderNote": order.orderNote ?? "",
+      "PaymentMethod": (order.isCashOnDelivery ?? true) ? "Cash" : "Online",
+      "OrderDate": DateTime.now().toIso8601String(),
+      "ItemCount": itemCount,
+      "TotalQuantity": totalQuantity,
+      "Items": order.cartEntity.cartItems
+          .map(
+            (e) => {
+              "Name": e.product.name,
+              "Quantity": e.quantity,
+              "Price": e.unitPrice,
+            },
+          )
           .toList(),
     };
-
-
     final jsonFile = File("order.json");
-
-    await jsonFile.writeAsString(
-      jsonEncode(json),
-      encoding: utf8,
-    );
-
-
+    await jsonFile.writeAsString(jsonEncode(json), encoding: utf8);
     final exe = File("printer/PrintService.exe");
-
-
     if (!exe.existsSync()) {
-      throw Exception(
-        "PrintService.exe not found: ${exe.absolute.path}",
-      );
+      throw Exception("PrintService.exe not found: ${exe.absolute.path}");
     }
-
-
-    final result = await Process.run(
+    Process.start(
       exe.absolute.path,
       [jsonFile.absolute.path],
     );
-
-
-    print("Exit Code: ${result.exitCode}");
-
-    print("STDOUT:");
-    print(result.stdout);
-
-    print("STDERR:");
-    print(result.stderr);
   }
 }
